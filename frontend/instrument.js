8bcf0c3a0e36d34ca1133a5f519da957f10af0ff
paulj <jaffrepaul@gmail.com>
2025-11-21 11:45:11 -0500

    instrumnet sentryt react sdk

---
+++ frontend/instrument.js
@@ -0,0 +1,34 @@
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://5909dbb9e8ba4bf105f58be39fc08ed2@o4509013641854976.ingest.us.sentry.io/4510403762323456",
+  // Adds request headers and IP for users, for more info visit:
+  // https://docs.sentry.io/platforms/javascript/guides/react/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+  integrations: [
+    // If you're using react router, use the integration for your react router version instead.
+    // Learn more at
+    // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for tracing.
+  // Learn more at
+  // https://docs.sentry.io/platforms/javascript/configuration/options/#traces-sample-rate
+  tracesSampleRate: 1.0,
+  // Set `tracePropagationTargets` to control for which URLs trace propagation should be enabled
+  tracePropagationTargets: [
+    /^\//,                          // Keep this for any same-origin requests
+    /^http:\/\/localhost:3000/,     // Add this for your local API
+    // /^https:\/\/api\.yourdomain\.com/, // Add production URL when deploying
+  ],
+
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  // Learn more at
+  // https://docs.sentry.io/platforms/javascript/session-replay/configuration/#general-integration-configuration
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,28 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+
+// import { StrictMode } from 'react';
+// import { createRoot } from 'react-dom/client';
+// import App from './App.jsx';
+
+// createRoot(document.getElementById('root')).render(
+  //   <StrictMode>
+  //     <App />
+  //   </StrictMode>
+  // );
+  
+import "../instrument";
+import { createRoot } from "react-dom/client";
+import * as Sentry from '@sentry/react';
 import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const container = document.getElementById('root');
+const root = createRoot(container, {
+  // Callback called when an error is thrown and not caught by an ErrorBoundary.
+  onUncaughtError: Sentry.reactErrorHandler((error, errorInfo) => {
+    console.warn('Uncaught error', error, errorInfo.componentStack);
+  }),
+  // Callback called when React catches an error in an ErrorBoundary.
+  onCaughtError: Sentry.reactErrorHandler(),
+  // Callback called when React automatically recovers from errors.
+  onRecoverableError: Sentry.reactErrorHandler(),
+});
+root.render(<App />);
